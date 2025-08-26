bb5a4ae961475d6a0b99dab4c0ed4f24653438f5
muugii-sudo <muugiio3u@gmail.com>
2025-08-27 00:24:46 +0200

    Made scenery

--- thegame/player.gd
+++ thegame/player.gd
@@ -12,12 +12,15 @@ var yaw_input := 0.0
 var is_ragdoll := false
 var is_grappling := false
 var grapple_point: Vector3
+var health = 1
 
 @onready var cam := $Camera3D
 @onready var right_arm := $RightArm
 @onready var left_arm := $LeftArm
 @onready var ground_check := $GroundRayCast
 @onready var wall_check := $WallRayCast
+@onready var death_screen: ColorRect = $"../CanvasLayer/ColorRect"
+
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -59,6 +62,19 @@ func _integrate_forces(_state):
 	if is_ragdoll:
 		return
 	
+	var is_on_ground = ground_check.is_colliding()
+	var was_on_ground = false
+	var start_height = 0
+	var safe_speed = 30
+	
+	if !was_on_ground and is_on_ground:
+		var fall_speed = abs(linear_velocity.y)
+		if fall_speed > safe_speed:
+			var speed_dif = fall_speed - safe_speed
+			var damage = speed_dif
+			apply_fall_damage(damage)
+	was_on_ground = is_on_ground
+	
 	var ang_vel = angular_velocity
 	ang_vel.y = yaw_input * 60
 	angular_velocity = ang_vel
@@ -87,6 +103,18 @@ func _integrate_forces(_state):
 	
 	gravity_scale = 2
 
+func apply_fall_damage(damage):
+	health -= damage
+	if health <= 0:
+		die()
+		print("YOU DIED")
+
+func die():
+	linear_velocity = Vector3.ZERO
+	is_ragdoll = true
+	death_screen.visible = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
 func toggle_ragdoll():
 	is_ragdoll = !is_ragdoll
 	if is_ragdoll:
@@ -100,3 +128,7 @@ func lock_upright():
 	axis_lock_angular_z = true
 	rotation_degrees.x = 0
 	rotation_degrees.z = 0
+
+
+func _on_button_pressed() -> void:
+	get_tree().reload_current_scene()
